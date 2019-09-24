e988aecebeaaa3ab2d8ade0828e95ebcbe122ab7
Brandon Furr <brandon.furr123@gmail.com>
2019-09-24 20:20:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  i = 1
+  names.each do |a|
+    puts "#{i} #{a}"
+    i += 1 
+  end
+
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  word = calls.map(&:capitalize)
+  
+  calls.collect.with_index do |calls|
+    word << "!"
+    
+  end
 end
+  
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  if array.each.with_index.any? > 
+    return 
+    
+  end
 end
 
 def find_the_cheese# code an argument here
